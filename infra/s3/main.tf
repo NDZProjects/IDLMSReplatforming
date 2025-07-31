c594024349a8b70b60d638ae1f071851194be9c8
sharon-ndz <sharon@ndz.co>
2025-07-31 21:28:34 +0530

    Update main.tf

--- infra/s3/main.tf
+++ infra/s3/main.tf
@@ -1,21 +1,12 @@
-resource "aws_s3_bucket" "docker_backup" {
-  bucket = var.s3_bucket_name
-  force_destroy = true
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket" "artifact" {
+  bucket = "idlms-${var.environment}-built-artifact-${data.aws_caller_identity.current.account_id}"
+
   tags = {
-    Name        = "IDLMS Docker Backup"
+    Name        = "IDLMS ${var.environment} Artifact Bucket"
     Environment = var.environment
   }
 
-
-}
-
-
-
-resource "aws_s3_bucket_public_access_block" "docker_backup_block" {
-  bucket = aws_s3_bucket.docker_backup.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  force_destroy = true
 }
