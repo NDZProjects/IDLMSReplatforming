e984b30522c596c2a57026e8332874b682571374
sharon-ndz <sharon@ndz.co>
2025-08-01 22:40:35 +0530

    Update and rename backend to backend.tf

---
+++ infra/ecr/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {}
+ #   bucket  = "my-terraform-state-bckt4321"   
+ #   key     = "stage-ndz/ecr/terraform.tfstate"       
+  #  region  = "eu-west-1"
+ #   encrypt = true
+ # }
+}
