473ca78a72b1a84cd5ee47855e9ee9e2f67dbb5d
sharon-ndz <sharon@ndz.co>
2025-08-02 14:53:01 +0530

    Update backend.tf

--- infra/cloudwatch/backend.tf
+++ infra/cloudwatch/backend.tf
@@ -1,7 +1,7 @@
 terraform {
-  backend "s3" {}
-#    bucket = "my-terraform-state-bckt4321"
-#    key    = "stage-ndz/cloudwatch/terraform.tfstate"
-#    region = "eu-west-1"
-#  }
+  backend "s3" {
+   bucket = "my-terraform-state-bckt4321"
+    key    = "stage-ndz/cloudwatch/terraform.tfstate"
+    region = "eu-west-1"
+  }
 }
